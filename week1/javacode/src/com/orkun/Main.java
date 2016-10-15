5b9bd0b9d1e9ea94bfb47210411751e2de61d4f8
orkun <kulceorkunfun@gmail.com>
2016-10-16 01:48:14 +0200

    last shit

---
+++ week1/javacode/src/com/orkun/Main.java
@@ -0,0 +1,30 @@
+package com.orkun;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            URL unibz = new URL("https://next.unibz.it/en");
+            URLConnection connection = unibz.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String input;
+            while((input = in.readLine()) != null ){
+                System.out.println(input);
+            }
+
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+
+    }
+}
